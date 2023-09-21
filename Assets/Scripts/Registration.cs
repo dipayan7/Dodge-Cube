088c193c4903dc767dd3173378bffe15ca9bbb5a
Dipayan Dasgupta <84182999+dipayan7@users.noreply.github.com>
2023-09-21 16:52:19 +0530

    regis  commit

--- Assets/Scripts/Registration.cs
+++ Assets/Scripts/Registration.cs
@@ -1,15 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 
 public class Registration : MonoBehaviour
 {
     public InputField nameField;
     public InputField passwordField;
+    public Text resultText;
 
     public Button submitButton;
+    private string phpScriptURL = "http://localhost/sqlconnect/register1.php";
 
     public void CallRegister()
     {
@@ -20,20 +22,74 @@ public class Registration : MonoBehaviour
         WWWForm form = new WWWForm();
         form.AddField("name", nameField.text);
         form.AddField("password", passwordField.text);
-        WWW www = new WWW("https://localhost/sqlconnect/register.php",form);
-        yield return www;
-        if (www.text == "0")
+        UnityWebRequest www = UnityWebRequest.Post(phpScriptURL, form);
+
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
         {
-            Debug.Log("User created successfully");
-            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            Debug.LogError("HTTP request error: " + www.error);
         }
         else
         {
-            Debug.Log("User creation Failed. Error # " + www.text);
+            string responseText = www.downloadHandler.text;
+            resultText.text = responseText;
+
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
         }
+
     }
+
     public void VerifyInputs()
     {
         submitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
     }
 }
+
+
+
+
+
+//using System.Collections;
+//using UnityEngine;
+//using UnityEngine.Networking;
+//using UnityEngine.UI;
+
+//public class DatabaseLogin : MonoBehaviour
+//{
+//    public InputField nameInputField;
+//    public InputField passwordInputField;
+//    public Text resultText;
+
+//    private string phpScriptURL = "https://yourwebsite.com/path/to/database.php";
+
+//    public void AttemptLogin()
+//    {
+//        StartCoroutine(SendRequest());
+//    }
+
+//    IEnumerator SendRequest()
+//    {
+//        // Create a form for POST data
+//        WWWForm form = new WWWForm();
+//        form.AddField("name", nameInputField.text);
+//        form.AddField("password", passwordInputField.text);
+
+//        // Create a UnityWebRequest with the POST method
+//        UnityWebRequest www = UnityWebRequest.Post(phpScriptURL, form);
+
+//        yield return www.SendWebRequest();
+
+//        if (www.result != UnityWebRequest.Result.Success)
+//        {
+//            Debug.LogError("HTTP request error: " + www.error);
+//        }
+//        else
+//        {
+//            string responseText = www.downloadHandler.text;
+//            resultText.text = responseText;
+
+//            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+//        }
+//    }
+//}
